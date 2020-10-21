3d4c1515025b91a0e9eee5b28df900890b8f6fe5
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-10-21 18:33:23 -0500

    :heavy_plus_sign: views: Se agrego la vista para el Login.

---
+++ src/views/Login.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="">
+        <h1>Login</h1>
+        <form @submit.prevent="login(usuario)" action="">
+            <div class="">
+                <label for="">Username:</label>
+            <input v-model="usuario.username" type="text"> 
+            </div>
+            <div class="">
+                <label for="">Password:</label>
+                <input v-model="usuario.password" type="password"> 
+            </div>
+            <button>Confirmar</button>
+        </form>
+        <div class="">
+          <h5>{{ usuario}}</h5>
+        </div>
+        <div class="">
+            <router-link to="/signup">Sign up</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data(){
+    return {
+      usuario: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
