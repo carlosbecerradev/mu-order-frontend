48da94d9d6a6062831b2bea00662dc344d37d777
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-10-23 02:46:00 -0500

    :heavy_plus_sign: App: Se agrego el 'router-link' para la vista de pedidos.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
+      <router-link to="/orders">Orders</router-link> |
       <router-link to="/login">Login</router-link> |
       <button @click="logout">Logout</button>
     </div>
