cbc544eaba4d0bbef2a0ee9ce1cbf2ac85a63df3
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-10-23 02:47:58 -0500

    :heavy_plus_sign: views: Se implementó el método 'getOrders' que pide a la API los pedidos disponibles.

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Pedidos</h1>
+  <h1>Pedidos</h1>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["token"]),
+  },
+  methods: {
+    async getOrders() {
+      try {
+        const response = await fetch("http://localhost:8090/api/order", {
+          method: "GET",
+          headers: {
+            'Authorization': "Bearer " + this.token,
+          },
+        });
+
+        const responseBody = await response.json();
+        console.log(responseBody);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  created() {
+    this.getOrders();
+  },
+};
+</script>
