f814d44323a6ce7992fb8346750430d10a0a65a5
Carlos Becerra <carlosbecerra.dev@gmail.com>
2020-11-10 00:55:51 -0500

    :heavy_plus_sign: views (MyOrderHistory): Traendo el historial de pedidos desde el 'store'.

--- src/views/MyOrderHistory.vue
+++ src/views/MyOrderHistory.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="">
     <h1>Order History</h1>
+    {{ myOrderHistory }}
   </div>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(["myOrderHistory"]),
+  },
+  methods: {
+    ...mapActions(["getMyOrderHistory"]),
+  },
+  created() {
+    this.getMyOrderHistory();
+  }
+}
+</script>
